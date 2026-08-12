17995004f3a737489b313b5bb3ad7e07194dfae4
Umair Rehman <umairrehman66@gmail.com>
2026-08-12 12:47:43 +0100

    added dynamic name

--- src/components/home-header.tsx
+++ src/components/home-header.tsx
@@ -1,13 +1,15 @@
 "use client"
 
-import { UserButton } from "@clerk/nextjs"
+import { UserButton, useUser } from "@clerk/nextjs"
 
 export function HomeHeader({ day }: { day: number }) {
+
+  const user = useUser()
   return (
     <header className="relative">
       <div className="min-w-0 text-center">
         <p className="text-base font-light tracking-wide text-muted-foreground">
-          Hi Umair,
+        Hi {user.user?.firstName ?? "there"},
         </p>
         <h1 className="mt-1 text-4xl font-extrabold tracking-tight sm:text-5xl">
           You have read Quran for <span className="text-foreground">{day} days</span>
